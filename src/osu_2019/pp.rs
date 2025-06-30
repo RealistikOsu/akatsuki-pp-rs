22693018b9d7ac0c33ff765b09bc985a6fecc7d9
Aochi <me@aochi.uk>
2025-06-30 02:49:30 +0200

    remove: length bonus

--- src/osu_2019/pp.rs
+++ src/osu_2019/pp.rs
@@ -300,12 +300,6 @@ impl<'m> OsuPP<'m> {
 
         let mut aim_value = (5.0 * (raw_aim / 0.0675).max(1.0) - 4.0).powi(3) / 100_000.0;
 
-        // Longer maps are worth more
-        let len_bonus = 0.88
-            + 0.4 * (total_hits / 2000.0).min(1.0)
-            + (total_hits > 2000.0) as u8 as f32 * 0.5 * (total_hits / 2000.0).log10();
-        aim_value *= len_bonus;
-
         // Penalize misses
         if effective_miss_count > 0.0 {
             let miss_penalty = self.calculate_miss_penalty(
